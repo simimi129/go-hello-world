d04aa9409d5c980c84e8ffe464ac0350114bc619
simimi129 <simimi129@gmail.com>
2023-01-05 16:40:51 +0100

    server

--- main.go
+++ main.go
@@ -3,27 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
-
-func Home(w http.ResponseWriter, r *http.Request){
-	renderTemplate(w, "home.page.html")
-}
-
-func About(w http.ResponseWriter, r *http.Request){
-	renderTemplate(w, "about.page.html")
-}
-
-func renderTemplate (w http.ResponseWriter, tmpl string){
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
-}
 
 func main() {
 	http.HandleFunc("/", Home)
